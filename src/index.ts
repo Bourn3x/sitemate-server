46b728258139413ab42c95fe545b959afe145b65
Julian Chong <julianchong10@gmail.com>
2024-03-21 15:20:26 +0800

    Fix issue where JSON could not be sent to POST request body

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import routes from "./routes";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = 3001;
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/api/v1", routes);
 
 app.listen(port, () => {
